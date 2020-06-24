2a97c02b12cc9ed9c82207e76dec1dfcf6b54073
johngodlee <johngodlee@gmail.com>
2020-06-24 19:57:06 +0100

    Cleaned repo

--- misc/ai_crop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-gdalwarp -te -5.939552 -36.684542 50.163780 6.391300 -overwrite data/ai_et0/ai_et0.tif data/ai_et0_crop/ai_et0_crop.tif 
--- misc/prod_calc/data_download_scripts_metadata/grb_to_nc.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for file in *.grb; do
-    cdo -f nc4 copy $file "$(basename "$file" .grb).nc"
-done
-

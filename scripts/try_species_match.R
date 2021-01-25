a786377d39d9900b4772a9c9ea6ceffe913acf97
johngodlee <johngodlee@gmail.com>
2021-01-25 10:56:01 +0000

    Added TRY species matching

---
+++ scripts/try_species_match.R
@@ -0,0 +1,24 @@
+# Which species are present in the TRY database?
+# John Godlee (johngodlee@gmail.com)
+# 2021-01-25
+
+# Import data
+try_sp <- read.table("data/try_species_list.txt", header = TRUE, sep = "\t")
+
+seosaw_stems <- read.csv("~/git_proj/seosaw_data/data_out/v2.10/stems_v2.10.csv")
+
+# Extract species from SEOSAW
+seosaw_sp <- unique(seosaw_stems$species_name_clean)
+
+# Match SEOSAW species with TRY database
+try_match <- try_sp[try_sp$AccSpeciesName %in% seosaw_sp,]
+
+# Which species aren't matched
+seosaw_no_match <- seosaw_sp[!seosaw_sp %in% try_sp$AccSpeciesName]
+
+# Check all species either matched or not matched
+stopifnot(length(seosaw_no_match) + nrow(try_match) == nrow(seosaw_sp))
+
+# Write matched species IDs to file for upload to TRY website
+writeLines(paste(try_match$AccSpeciesID, collapse = ","), "data/try_match.txt")
+
